5fd47950c181ca6c9c5a9774e9661d368f7c4234
Vik Kumar <vik.ceo@gmail.com>
2012-07-31 14:51:26 -0700

    Finish user edit, update, index and destory

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ SampleApp::Application.routes.draw do
   match '/signup', to: 'users#new'
   match '/signin',  to: 'sessions#new'
   match '/signout', to: 'sessions#destroy', via: :delete
+  match '/users', to:'users#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
