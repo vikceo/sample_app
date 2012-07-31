606db7bb42c5de1b87c58138ea8b150c039c5a9a
Vik Kumar <vik.ceo@gmail.com>
2012-07-31 16:01:34 -0700

    Add user microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -16,6 +16,12 @@ module SessionsHelper
       user == current_user
     end
 
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_path, notice: "Please sign in."
+    end
+  end
 
   def signed_in?
     !current_user.nil?
